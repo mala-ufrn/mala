923eece92528b600f7b1eca1aafa71de4e48d65a
Paulo Lopes <paulo.hq.lopes@gmail.com>
2017-10-25 11:35:05 +0000

    Introducing BarCode Reader <not worked>

--- app/src/main/java/br/ufrn/mala/activity/NovoEmprestimoActivity.java
+++ app/src/main/java/br/ufrn/mala/activity/NovoEmprestimoActivity.java
@@ -1,15 +1,17 @@
 package br.ufrn.mala.activity;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import br.ufrn.mala.R;
 
 public class NovoEmprestimoActivity extends AppCompatActivity {
+
+    private static final int RC_BARCODE_CAPTURE = 9001;
+    private static final String TAG = "BarcodeMain";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +24,19 @@ public class NovoEmprestimoActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "Chamar a camera", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                camReader(v);
             }
 
         });
+
+
+    }
+
+
+    public void camReader(View v) {
+        Intent intent = new Intent(this, CameraActivity.class);
+
+        startActivity(intent);
     }
 
 
