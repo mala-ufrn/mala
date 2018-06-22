06b7737269aa2f6ea0d8b75ff99a6e397ce166aa
Joel Felipe <joelfelipe07@gmail.com>
2018-06-22 00:04:09 -0300

    Update url autentication

--- app/src/main/java/br/ufrn/mala/util/Constants.java
+++ app/src/main/java/br/ufrn/mala/util/Constants.java
@@ -11,8 +11,8 @@ public class Constants {
     public static final String SECRET_KEY = "segredo";
 
     public static final String REDIRECT_URI = "https://api.ufrn.br";
-    public static final String AUTHORIZATION_URL = "https://apitestes.info.ufrn.br/authz-server/oauth/authorize";
-    public static final String ACCESS_TOKEN_URL = "https://apitestes.info.ufrn.br/authz-server/oauth/token";
+    public static final String AUTHORIZATION_URL = "https://autenticacao.info.ufrn.br/authz-server/oauth/authorize";
+    public static final String ACCESS_TOKEN_URL = "https://autenticacao.info.ufrn.br/authz-server/oauth/token";
     public static final String RESPONSE_TYPE_PARAM = "response_type";
     public static final String RESPONSE_TYPE_AUTHORIZATION = "code";;
     public static final String RESPONSE_TYPE_REFRESH = "refresh_token";
